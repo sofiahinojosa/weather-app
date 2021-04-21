a5fe440265e3711a3ac257008e77d0d3b77f05bd
Sofia Hinojosa <sofiahinojosadl@gmail.com>
2021-04-21 12:45:41 -0500

    forecast

--- index.js
+++ index.js
@@ -95,27 +95,36 @@ nameIdk();
 
 // forecast :3
 
+function formatDay(timestap) {
+  let date= new Date(timestap * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day]
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let dayz = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  //let dayz = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
 
   let forecastHTML = `<div class="row">`;
-  dayz.forEach(function (date) {
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `
       <div class="col-2">
-        <div class="weather-forecast-date">${date}</div>
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
+          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max"> ${forecastDay.temp.max} </span>
+          <span class="weather-forecast-temperature-min"> ${forecastDay.temp.min} </span>
         </div>
       </div>
   `;
